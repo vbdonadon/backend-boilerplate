fe5b727aff372e8f36691ef1111778a6ab183a69
Vinicius <vinicius.donadon@genialinvestimentos.com.br>
2023-02-19 17:03:50 -0300

    Minor changes

---
+++ prisma/migrations/20230219190337_yarn_prisma_studio/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `clients` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `deliveries` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `deliveryman` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "deliveries" DROP CONSTRAINT "deliveries_id_client_fkey";
+
+-- DropForeignKey
+ALTER TABLE "deliveries" DROP CONSTRAINT "deliveries_id_deliveryman_fkey";
+
+-- DropTable
+DROP TABLE "clients";
+
+-- DropTable
+DROP TABLE "deliveries";
+
+-- DropTable
+DROP TABLE "deliveryman";
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
